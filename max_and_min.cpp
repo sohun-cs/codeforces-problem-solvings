b8d2bae0f3ce04cc9eab8e86e333380e71e4c559
sohun-cs <asaduzzamansohun5@gmail.com>
2025-07-03 12:46:29 +0600

    Min and Max

---
+++ max_and_min.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    cout << min({A, B, C}) << " " << max({A, B, C});
+
+    return 0;
+}
